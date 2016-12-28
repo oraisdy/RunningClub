751e87870947ffd6320d4e320aaf249e4ef3ad53
oraisdy <569450503@qq.com>
2016-12-28 14:38:36 +0800

    update register

--- controller/register.php
+++ controller/register.php
@@ -1,13 +1,33 @@
 <?php
 
 
-
+$Expires = 30;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $Result = $DB->query('insert into `user`(login,password,email,joinAt) values(?,?,?,?);',
-        array($_POST['login'],$_POST['password'],$_POST['email'],$CurDate));
+    $Result = $DB->query('insert into `user`(login,password,email,joinAt,avatar_url) values(?,?,?,?,?);',
+        array($_POST['login'],$_POST['password'],$_POST['email'],$CurDate,"/static/image/avatar7.jpg"));
 
-    echo $Result;
+    do {
+
+        echo ($Result);
+
+        if ($Result != 1) {
+            break;
+        }
+
+        $DBUser = $DB->row("select * from user where email=:email", array("email" => $_POST['email']));
+
+        $TemporaryUserExpirationTime = $Expires * 86400 + $TimeStamp;
+
+        SetCookies(array(
+            'UserID' => $DBUser['id'],
+            'UserName' => $DBUser['login'],
+            'UserExpirationTime' => $TemporaryUserExpirationTime,
+        ), $Expires);
+
+        header('location:/');
+
+    } while (false);
 }
 
 
